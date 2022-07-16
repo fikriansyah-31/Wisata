aded2449799013c60f3d89cc38bcd5907adbdca6
fikriansyah-31 <syafanurwalinda23@gmail.com>
2022-07-16 21:08:18 +0700

    finish

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ const app = express()
 
 const server = http.createServer(app)
 
-// const io = new Server(server, {
-//     cors: {
-//         origin: 'http://localhost:3000' // define client origin if both client and server have different origin
-//     }
-// })
 
 
 
